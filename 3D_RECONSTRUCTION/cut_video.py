e7297b066cbf51117ff933e5669d1c33c68d3027
Ксения Андрющенко <ksandrstudy@gmail.com>
2024-05-28 03:28:12 +0300

    3d reconstraction add

--- 3D_RECONSTRUCTION/cut_video.py
+++ 3D_RECONSTRUCTION/cut_video.py
@@ -4,8 +4,8 @@ import os
 def extract_frames(video_path, N):
     print(os.getcwd())
     # Проверяем существует ли папка 'frames', если нет - создаем
-    if not os.path.exists('frames'):
-        os.makedirs('frames')
+    if not os.path.exists('data/input/'):
+        os.makedirs('data/input/')
 
     # Загружаем видео
     cap = cv2.VideoCapture(video_path)
@@ -28,8 +28,8 @@ def extract_frames(video_path, N):
         # Если кадр успешно прочитан
         if ret:
             if current_frame % frame_interval == 0 and saved_frames < N:
-                # Сохраняем кадр в папке 'frames' с именем 'frame(номер).png'
-                cv2.imwrite(f'frames/frame{saved_frames}.png', frame)
+                # Сохраняем кадр в папке 'data/input' с именем 'frame(номер).png'
+                cv2.imwrite(f'data/input/frame{saved_frames}.png', frame)
                 saved_frames += 1
             current_frame += 1
         else:
@@ -43,5 +43,37 @@ def extract_frames(video_path, N):
     cap.release()
     print(f"Сохранено {saved_frames} кадров.")
 
+def read_config(file_path, parameter):
+    """
+    Функция для чтения конфигурационного файла и извлечения значения заданного параметра.
 
-extract_frames('playground.mp4', 200)
+    :param file_path: Путь к конфигурационному файлу.
+    :param parameter: Название параметра, значение которого нужно извлечь.
+    :return: Значение параметра, если найдено, иначе None.
+    """
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Удаление пробельных символов в начале и конце строки
+            line = line.strip()
+            # Пропуск комментариев и пустых строк
+            if line.startswith('#') or not line:
+                continue
+            # Разделение строки на параметр и значение
+            if '=' in line:
+                param, value = map(str.strip, line.split('=', 1))
+                if param == parameter:
+                    # Удаление кавычек, если значение заключено в кавычки
+                    if value.startswith('"') and value.endswith('"'):
+                        value = value[1:-1]
+                    return value
+    return None
+
+# Путь к конфигурационному файлу
+config_file_path = '../config.txt'
+param_to_extract = 'video_name'
+frames_parametrs = 'frames'
+video_name = read_config(config_file_path, param_to_extract)
+frames = read_config(config_file_path, frames_parametrs)
+print(f"The value of '{param_to_extract}' is: {video_name}, '{frames_parametrs}' is {frames}")
+# Нарезка видео на заданное колличество кадров
+extract_frames("input_data/"+video_name, int(frames))
